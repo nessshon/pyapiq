368b9695a9f6990c5cc130710197ba416edba848
ness <shonness98@gmail.com>
2025-08-25 13:03:15 +0500

    Update version number to `0.1.1`.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyapiq",
-    version="0.1.0",
+    version="0.1.1",
     author="nessshon",
     description="PyAPIq is a modern Python toolkit for building both synchronous and asynchronous API clients with clean, minimal code and full type safety.",
     long_description=long_description,
